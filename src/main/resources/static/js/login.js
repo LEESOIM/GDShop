a3fcc24df69c8bafebec104ed0dd53c164dfe261
KimSeolHee <tjfgml64@nate.com>
2022-11-18 16:58:30 +0900

    아이디 비번찾기

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,24 +1,23 @@
-//로그인
-// log_btn.addEventListener("click", function () {
-//   const xhttp = new XMLHttpRequest();
-//   xhttp.open("POST", "/member/login");
-//   xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-//   xhttp.send("id=" + id + "&pw=" + pw);
-//   xhttp.onreadystatechange = function () {
-//     if (this.readyState == 4 && this.status == 200) {
-//       if (xhttp.responseText == 1) {
-//         window.location.reload();
-//       } else {
-//         alert("정보가 다릅니다!");
-//       }
-//     }
-//   };
-// });
-
-//jquery
+//로그인 (jquery)
 $("#log_btn").click(function () {
   let id = $("#id").val();
   let pw = $("#pw").val();
   console.log(id);
   console.log(pw);
+
+  $.ajax({
+    type: "POST",
+    url: "/member/login",
+    data: {
+      id: id,
+      pw: pw,
+    },
+    success: function (data) {
+      if (data == 1) {
+        window.location.reload();
+      } else {
+        alert("로그인 실패");
+      }
+    },
+  });
 });
