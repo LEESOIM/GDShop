f1a61e9ea5c581c1eb530e5198bca772d24951ef
junsolhee7@naver.com <junsolhee7@naver.com>
2022-11-28 17:46:19 +0900

    1

--- src/main/resources/static/js/seller.js
+++ src/main/resources/static/js/seller.js
@@ -1,17 +1,3 @@
-swal({
-	title: "입점 신청 하시겠습니까?",
-	icon: "info", //warning,error,success,info,question
-	buttons: true,
-	dangerMode: true
-})
-.then((asd)=>{
-	if(asd){
-		console.log("sibal");
-	}else{
-		console.log("nonononononono");
-	}
-})
-
 //$("*") 전체
 //$("#아이디명") 아이디
 //$(".클래스명") 클래스
@@ -32,7 +18,7 @@ window.addEventListener('load', () => {
 	});
 }, false);
 
-$("#standingRequest").on("click",function(e){
+$("#standingRequest").on("click", function(e) {
 	console.log("================================");
 	let id = $("#id1").val();
 	let name = $("#name").val();
@@ -41,45 +27,45 @@ $("#standingRequest").on("click",function(e){
 	let company = $("#company").val();
 	let agree = $("#aggrement").is(':checked');
 
-	console.log("1.ID명:"+id);
-	console.log("2.이름:"+name);
-	console.log("3.이메일:"+email);
-	console.log("4.전화번호:"+phone);
-	console.log("5.기업명:"+company);
-	console.log("6.동의:"+agree);
-	
+	console.log("1.ID명:" + id);
+	console.log("2.이름:" + name);
+	console.log("3.이메일:" + email);
+	console.log("4.전화번호:" + phone);
+	console.log("5.기업명:" + company);
+	console.log("6.동의:" + agree);
 
-	if(id!=null && name!=null && email!=null && phone!=null && company!="" && agree){
-		swal({
-			title: "입점 신청 하시겠습니까?",
-			icon: "info", //warning,error,success,info,question
-			buttons: true,
-			dangerMode: true
-		})
-		.then((qwer) => {
-			if (qwer) {
-				// $.ajax({
-				// 	type:"post",
-				// 	url:"/seller/seller",
-				// 	traditional:true,
-				// 	contentType:'application/json',
-				// 	data:JSON.stringify({
-				// 		"id":id,
-				// 		"roleNum":"4",
-				// 		"name":name,
-				// 		"email":email,
-				// 		"phone":phone,
-				// 		"company":company
-				// 	}),
-				// 	success:function(response){
-				// 		console.log("================ajax성공");
-				// 	},
-				// 	error:function(){
-				// 		console.log("================ajax실패");
-				// 	}
-				// })
-			}else{
-				console.log("asd");
+	if (id != null && name != null && email != null && phone != null && company != "" && agree) {
+
+		$.ajax({
+			type: "post",
+			url: "/seller/seller",
+			traditional: true,
+			contentType: 'application/json',
+			data: JSON.stringify({
+				"id": id,
+				"roleNum": "4",
+				"name": name,
+				"email": email,
+				"phone": phone,
+				"company": company
+			}),
+			success: function(response) {
+				swal({
+					title: "입점 신청이 완료되었습니다.",
+					icon: "success", //warning,error,success,info,question
+					buttons: true,
+					dangerMode: true
+				})
+				console.log("================ajax성공");
+			},
+			error: function() {
+				swal({
+					title: "입점 신청이 실패하였습니다.",
+					icon: "error", //warning,error,success,info,question
+					buttons: true,
+					dangerMode: true
+				})
+				console.log("================ajax실패");
 			}
 		})
 	}
